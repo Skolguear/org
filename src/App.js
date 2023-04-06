ba38ffe37d43d3348fd4e5a1ed5d6a0feb880f00
Rafael Ara <rafael.ara.casanova@gmail.com>
2023-04-06 01:01:35 -0600

    Proyecto React

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Header from './components/Header/Header';
+
+/*Los apartados de Header se mandan a llamar con llaves a la funcion {Header()} o con las etiquetas
+mismas de la funcion y se reconoce con la letra mayuscula <Header></Header> y con <Header/>
+*/
+function App() {
+  return (
+    <div>
+      <Header/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import "./Header.css"
+
+function Header() {
+    return <header className="header">
+        <img src='/img/Header.png' alt="Org"/>
+    </header> 
+}
+
+export default Header
+
+/*Los componentes se hacen en funciones que retornar las etiquetas html
+y a su vez se deven expoertar como se muestra en este ejemplo
+las funciones se deben hacer con la primera letra en Mayuscula para poder reconocerla
+*/
